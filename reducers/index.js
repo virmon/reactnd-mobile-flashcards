a718dbc28847dd43eb0bfc8c31a1e89ce3445d67
virmon <virmontumulak@gmail.com>
2019-05-02 21:11:10 +0800

    feat: create reducer

---
+++ reducers/index.js
@@ -0,0 +1,28 @@
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions'
+
+function deck (state = {}, action) {
+    switch (action.type) {
+        case RECEIVE_DECKS :
+            return {
+                ...state,
+                ...action.decks
+            }
+        case ADD_DECK :
+            return {
+                ...state,
+                ...action.deck
+            }
+        case ADD_CARD :
+            return {
+                ...state,
+                [action.deck]: {
+                    ...state.title,
+                    questions: state.questions.push(action.card)
+                }
+            }
+        default : 
+            return state
+    }
+}
+
+export default deck
